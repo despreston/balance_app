8cd3073fe6fb56956fb18e53ed208d2d7db61676
Des Preston <dpreston@enservio.com>
2015-02-16 16:53:37 -0500

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Desmond Preston on 2/16/15.
+//  Copyright (c) 2015 Desmond Preston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
